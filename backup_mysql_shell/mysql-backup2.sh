0d72867fead4c4fa8b2ad509a7f6bbb478b5c79a
makhloufi <soufianem370@gmail.com>
2019-10-18 20:03:31 +0200

    ajoute scripte backup db

---
+++ backup_mysql_shell/mysql-backup2.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+################################################################
+##
+##   MySQL Database Backup Script 
+##   Written By: Rahul Kumar
+##   URL: https://tecadmin.net/bash-script-mysql-database-backup/
+##   Last Update: Jan 05, 2019
+##
+################################################################
+
+export PATH=/bin:/usr/bin:/usr/local/bin
+TODAY=`date +"%d%b%Y%H%M%S"`
+
+################################################################
+################## Update below values  ########################
+server=$(hostname)
+DB_BACKUP_PATH='/backup/dbbackup'
+MYSQL_HOST='192.168.1.227'
+MYSQL_PORT='3306'
+MYSQL_USER='root'
+MYSQL_PASSWORD='root'
+DATABASE_NAME='unixmen'
+BACKUP_RETAIN_DAYS=30   ## Number of days to keep local backup copy
+
+#################################################################
+
+mkdir -p ${DB_BACKUP_PATH}/${TODAY}
+echo "Backup started for database - ${DATABASE_NAME}"
+
+
+mysqldump -h ${MYSQL_HOST} \
+		  -P ${MYSQL_PORT} \
+		  -u ${MYSQL_USER} \
+		  -p${MYSQL_PASSWORD} \
+		  ${DATABASE_NAME} | gzip > ${DB_BACKUP_PATH}/${TODAY}/${MYSQL_HOST}-${DATABASE_NAME}-${TODAY}.sql.gz
+
+if [ $? -eq 0 ]; then
+  echo "Database backup successfully completed"
+else
+  echo "Error found during backup"
+fi
+
+
+##### Remove backups older than {BACKUP_RETAIN_DAYS} days  #####
+
+DBDELDATE=`date +"%d%b%Y" --date="${BACKUP_RETAIN_DAYS} days ago"`
+
+if [ ! -z ${DB_BACKUP_PATH} ]; then
+      cd ${DB_BACKUP_PATH}
+      if [ ! -z ${DBDELDATE} ] && [ -d ${DBDELDATE} ]; then
+            rm -rf ${DBDELDATE}
+      fi
+fi
+
+### End of script ####
